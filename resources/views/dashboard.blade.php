662a8a7ae5910f6fb4e12a34fb7930af707866dc
Ashish Rana <ashishrana288@gmail.com>
2026-02-05 11:08:42 +0530

    added dashboard table view code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,9 +23,66 @@
     </div>
     <hr>
     <br>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-    <div id="top_x_div" style="width: 600px; height: 400px;"></div>
+    <div class="grid grid-cols-2 gap-6">
+    <div id="top_x_div" style="width: 600px; height: 480px;"></div>
+
+    <div class="overflow-x-auto">
+    <table class="min-w-full border border-gray-200 rounded-lg">
+        <thead class="bg-gray-100">
+            <tr>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                Pending Tasks
+                </th>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                Priority
+                </th>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                Assigned Date
+                </th>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                Assigned User	
+                </th>
+            </tr>
+        </thead>
+
+        <tbody class="bg-white">
+            <tr class="hover:bg-gray-50">
+                <td class="px-4 py-2 border-b">The Sliding</td>
+                <td class="px-4 py-2 border-b">High</td>
+                <td class="px-4 py-2 border-b">28-02-2026</td>
+                <td class="px-4 py-2 border-b">Ashish</td>
+            </tr>
+        </tbody>
+    </table>
+<br>
+    <table class="min-w-full border border-gray-200 rounded-lg">
+        <thead class="bg-gray-100">
+            <tr>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                Recently Added Tasks
+                </th>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                    Artist
+                </th>
+                <th class="px-4 py-2 text-left text-sm font-semibold text-gray-700 border-b">
+                    Year
+                </th>
+            </tr>
+        </thead>
+
+        <tbody class="bg-white">
+            <tr class="hover:bg-gray-50">
+                <td class="px-4 py-2 border-b">The Sliding</td>
+                <td class="px-4 py-2 border-b">Malcolm Lockyer</td>
+                <td class="px-4 py-2 border-b">1961</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
+
     </div>
+
    
 
 </div>
