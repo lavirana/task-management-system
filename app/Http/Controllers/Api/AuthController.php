aed4d704cecf2897cbb83809082a17acc3798249
Ashish Rana <ashishrana288@gmail.com>
2026-02-16 12:10:20 +0530

    added api code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,4 +23,20 @@ class AuthController extends Controller
     ]);
 
     }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['message' => 'Invalid credentials'],401);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged Out']);
+    }
 }
